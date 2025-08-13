e9d73fed6e5b74053da44e0e5231e99a6539aa67
django-json <diango.jayson@gmail.com>
2025-08-13 21:15:57 +0800

    agents setup

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import LoadingState from "@/components/loading-state";
+import ErrorState from "@/components/error-state";
+
+const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
+
+const AgentsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading State"
+      description="Something went wrong"
+    />
+  );
+};
+
+export { AgentsView, AgentsViewLoading, AgentsViewError };
