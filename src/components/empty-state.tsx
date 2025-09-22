19dca94532cd5ed8d5d4f68a820bbbe7b9b87878
Jayson Diango <diango.jayson@gmail.com>
2025-09-22 19:55:25 +0800

    meetings data table

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -12,7 +12,9 @@ const EmptyState = ({ title, description }: Props) => {
       <div className="flex flex-col items-center justify-center gap-y-6 max-2-md mx-auto text-center">
         <div className="flex flex-col gap-y-2 text-center">
           <h6 className="text-lg font-medium">{title}</h6>
-          <p className="text-sm text-muted-foreground">{description}</p>
+          <p className="text-sm text-muted-foreground max-w-lg">
+            {description}
+          </p>
         </div>
       </div>
     </div>
